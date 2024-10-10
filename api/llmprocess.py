d0e49eef0c83f1c7a02ed37a252f3e2351689a1c
gautam <gautambabariya0125@gmail.com>
2024-10-10 23:03:55 +0530

    first commit

---
+++ api/llmprocess.py
@@ -0,0 +1,43 @@
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import AutoTokenizer
+
+GOOGLE_API_KEY='AIzaSyACKz-i5GfZF3hbbtwV_ZvqaCnaokhnbUg'
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+
+def get_transcripts(youtube_url):
+    # Extract the video ID from the YouTube URL
+    video_id = youtube_url.split("=")[1]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'hi', 'gu'])
+    except Exception as e:
+        raise Exception(f"Error fetching transcript: {e}")
+
+    full_transcript = " ".join([entry['text'] for entry in transcript])
+    return full_transcript
+
+def chunk_text(text, max_token_length=4000):
+    # Tokenize the text
+    tokens = tokenizer.encode(text, add_special_tokens=False)
+
+    # Chunk tokens into max_token_length chunks
+    chunks = [tokens[i:i + max_token_length] for i in range(0, len(tokens), max_token_length)]
+
+    # Decode token chunks back into text
+    chunked_texts = [tokenizer.decode(chunk) for chunk in chunks]
+
+    return chunked_texts
+
+def generate_summary(text):
+    prompt = f"Summarize the following text with cover all context:\n\n{text}\n\nSummary:"
+    response = model.generate_content(prompt)
+    return response.text
+
+def final_summary(youtube_url):
+    full_transcript = get_transcripts(youtube_url)
+    chunks = chunk_text(full_transcript)
+    summaries = [generate_summary(chunk) for chunk in chunks]
+    return  " ".join(summaries)
