30c74c0c11eb3d9fb98df54822bd0b44804be2a7
gautam <gautambabariya0125@gmail.com>
2024-10-11 16:25:25 +0530

    tmbn

--- api/index.py
+++ api/index.py
@@ -1,6 +1,21 @@
 from flask import Flask, request, jsonify
-from .llmprocess import final_summary
-from .llmprocess import get_transcripts
+# from .llmprocess import final_summary
+# from .llmprocess import get_transcripts
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def get_transcripts(youtube_url):
+    # Extract the video ID from the YouTube URL
+    video_id = youtube_url.split("=")[1]
+
+    print(video_id)
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'hi', 'gu'])
+    except Exception as e:
+        raise Exception(f"Error fetching transcript: {e}")
+
+    full_transcript = " ".join([entry['text'] for entry in transcript])
+    return full_transcript
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -12,12 +27,14 @@ def home():
 
 @app.route('/generate', methods=['POST'])
 def generate():
+    
+
     try:
         # Extract the YouTube URL from the request
         data = request.json
         youtube_url = data.get('youtube_url')
 
-        summary = final_summary(youtube_url)
+        summary = get_transcripts(youtube_url)
         
         # Return the summary
         return jsonify({
